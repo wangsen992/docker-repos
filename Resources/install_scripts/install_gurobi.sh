2217bfc8067a18b016e81ec6696e1151311ae18d
Sen Wang <wangsen992@gmail.com>
2024-04-01 11:21:19 -0400

    minor update

--- Resources/install_scripts/install_gurobi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-wget https://packages.gurobi.com/10.0/gurobi10.0.3_linux64.tar.gz
-tar xzf gurobi10.0.3_linux64.tar.gz
--- bin/RunFunctions.sh
+++ bin/RunFunctions.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
+. ../etc/bashrc
+
 function hello_world
 {
 	echo hellow world
 }
+
+# Function used to create an app with a base OS configuration file
+# -base option will be either absolute or relative path to the OS 
+# -name will be the name of the app, can be a absolute or relative path too
+# Example: create_app -b ../Resource/OS/Ubuntu -n blender
+function create_app
+{
+	while getopts b:n: flag
+	do
+		case "${flag}" in
+			b) base=${OPTARG};;
+			n) name=${OPTARG};;
+		esac
+	done
+
+	echo "base=$base"
+	echo "name=$name"
+
+	# Create the app
+	cp -r $base $name
+
+}	
