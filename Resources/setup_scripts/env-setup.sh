99513fe6f4452894c40e4647e38b11569eb2d468
Sen Wang <wangsen992@gmail.com>
2024-04-11 18:31:46 +0000

    add deb building tools

--- Resources/setup_scripts/env-setup.sh
+++ Resources/setup_scripts/env-setup.sh
@@ -3,6 +3,10 @@
 # Set Envs
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/lib32:/usr/lib64:/usr/local/lib
 
+# deb building tools
+sudo apt install -y build-essential binutils lintian debhelper dh-make devscripts
+
+
 # Run install scripts
 # /bin/bash ${IMAGE_RESOURCES}/install_scripts/install_mpich33.sh
 /bin/bash ${IMAGE_RESOURCES}/install_scripts/install_essential.sh
@@ -17,3 +21,7 @@ sudo chmod -R 777 /home/${USER}
 # Put envs to bashrc
 echo "export USER=${USER}" >> /home/${USER}/.bashrc
 echo "export IMAGE_RESOURCES=${IMAGE_RESOURCES}" >> /home/${USER}/.bashrc
+
+# provide git info
+git config --global user.email "wangsen992@gmail.com"
+git config --global user.name "Sen Wang"
